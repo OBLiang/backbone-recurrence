9129a9617eb3d17d1b5d1b71ed2365592ea8e86b
Doobet <Doobet@163.com>
2020-04-15 11:47:32 +0800

    7th commit:结束卷积/反卷积中maxpooling/maxunpooling结合使用问题，抛弃sequential用法，权重随机且无训练，认为训练时可使用汉明距离

--- Deconvoluational_NN.py
+++ Deconvoluational_NN.py
@@ -6,14 +6,6 @@ import torch.nn as nn
 import torchvision.transforms as transform
 import cv2
 import numpy as np
-# origimg_path="./floder/50.jpg"
-# img=cv2.imread(origimg_path)
-# print(img.shape)
-# img=cv2.resize(img,(64,64),interpolation=cv2.INTER_CUBIC)
-# cv2.imwrite("./floder/50.jpg",img)
-# cv2.imshow("original_pic",img)
-# cv2.waitKey(0)
-# print(img.shape)
 '''
 predeal input img
 '''
@@ -29,76 +21,53 @@ print(tensor_img.shape)
 model define
 deconvoluational network based on Lenet
 '''
-class DLenet_convpart(nn.Module):
+class DLenet(nn.Module):
     def __init__(self):
-        super(DLenet_convpart, self).__init__()
-        self.conv=nn.Sequential(
-            nn.Conv2d(in_channels=3,
+        super(DLenet, self).__init__()
+        #conv1
+        self.conv1= nn.Conv2d(in_channels=3,
                       out_channels=6,
                       kernel_size=11,
                       stride=1,
                       padding=1,
-                      ),#64*3->56*6
-            # nn.MaxPool2d(2,return_indices=True),#56->28
-            # nn.Conv2d(in_channels=6,
-            #           out_channels=10,
-            #           kernel_size=9,
-            #           stride=1,
-            #           padding=1,
-            #           ),#50*10
-            # nn.MaxPool2d(2,return_indices=True),#20->10
-            # nn.Conv2d(in_channels=10,
-            #           out_channels=16,
-            #           kernel_size=9,
-            #           stride=1,
-            #           padding=1,
-            # ),#44*16
-        )
-    def forward(self,x):
-        x=self.conv(x)
-        # x=self.convtranspose(x)
-        return x
-class Dlent_deconvpart(nn.Module):
-    def __init__(self):
-        super(Dlent_deconvpart, self).__init__()
-        self.deconv=nn.Sequential(
-            # nn.ConvTranspose2d(
-            #     in_channels=16,
-            #     out_channels=10,
-            #     kernel_size=9,
-            #     stride=1,
-            #     padding=1,
-            # ),
-            # nn.ConvTranspose2d(
-            #     in_channels=10,
-            #     out_channels=6,
-            #     kernel_size=9,
-            #     stride=1,
-            #     padding=1,
-            # ),
-            nn.ConvTranspose2d(
+                      )#64*3->56*6c
+        # self.pool1= nn.MaxPool2d(2,2,return_indices=True)#56*6->28*6
+        # self.conv2= nn.Conv2d(in_channels=6,
+        #               out_channels=10,
+        #               kernel_size=9,
+        #               stride=1,
+        #               padding=1,
+        #               )#22*10
+        # self.deconv1= nn.ConvTranspose2d(
+        #         in_channels=10,
+        #         out_channels=6,
+        #         kernel_size=9,
+        #         stride=1,
+        #         padding=1,
+        #     )#28*6
+        # self.unpool1=  nn.MaxUnpool2d(2,2)#56*6
+        self.deconv2=  nn.ConvTranspose2d(
                 in_channels=6,
                 out_channels=3,
                 kernel_size=11,
                 stride=1,
                 padding=1,
-            ),
-        )
+            )#64*3
     def forward(self,x):
-        x=self.deconv(x)
-        return x
+        out=self.conv1(x)
+        # out,indice=self.pool1(out)
+        # out=self.conv2(out)
+        #
+        # out=self.deconv1(out)
+        # out=self.unpool1(out,indice)
+        out=self.deconv2(out)
+        return out
 
-convpart=DLenet_convpart()
-deconvpart=Dlent_deconvpart()
-conv_result=convpart(tensor_img)
-print(conv_result.shape)
-deconv_result=deconvpart(conv_result)
-deconv_result=deconv_result.squeeze(0)
-deconv_result=torch.transpose(deconv_result,0,2)
-print(deconv_result.shape)
-numpy_img=deconv_result.data.numpy()*255
+dlnet=DLenet()
+generate_result=dlnet(tensor_img)
+generate_result=torch.transpose(generate_result.squeeze(0),0,2)
+numpy_img=generate_result.data.numpy()*255
 print(numpy_img.shape)
-# numpy_img=cv2.merge([b,g,r])
-cv2.imshow("generate img",numpy_img)
+# cv2.imshow("generate img",numpy_img)
 cv2.imwrite("./generator/generator2.jpg",numpy_img)
 cv2.waitKey(0)
